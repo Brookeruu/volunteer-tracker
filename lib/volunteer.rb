f35b96b6864dd61a217121c697b77908b2a35d66
Brooke Perry <perrysbrooke@gmail.com>
2018-12-07 09:15:58 -0800

    pass spec: rtn volunteer\'s project_id

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,10 +1,12 @@
 class Volunteer
-  attr_reader :name, :id
+  attr_reader :name, :id, :project_id
 
   def initialize(attributes)
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id)
+    @project_id = attributes.fetch(:project_id)
   end
+
 
 
 
