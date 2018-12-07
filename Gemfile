a6959e3c7fce57110a5abee2d37af885fc723226
Brooke Perry <perrysbrooke@gmail.com>
2018-12-07 08:49:42 -0800

    add required specs

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+
+gem 'rspec'
+gem 'pry'
+gem 'capybara'
+gem 'pg'
+gem 'pivotal_git_scripts'
+gem 'sinatra'
+gem 'sinatra-contrib'
---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+require 'volunteer'
+require 'project'
+require 'pg'
+require 'pry'
+
+DB = PG.connect({:dbname => 'volunteer_tracker'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+     DB.exec("DELETE FROM projects *;")
+    DB.exec("DELETE FROM volunteers *;")
+  end
+end
