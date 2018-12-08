89c34b6c3d809a2919372d1510fc01306a832718
Brooke Perry <perrysbrooke@gmail.com>
2018-12-07 23:24:50 -0800

    update project title

--- app.rb
+++ app.rb
@@ -14,20 +14,34 @@ get '/' do
   erb(:index)
 end
 
-post('/') do
+post('/projects') do
   if params["title"]
     title = params["title"]
     project = Project.new(:title => title, :id => nil)
     project.save
   end
+  @id = project.id
   @projects = Project.all
   @volunteers = Volunteer.all
   erb(:index)
 end
 
 
-get('/projects') do
-  @projects = Project.all
+get('/projects/:id') do
+  @id = params[:id].to_i
+  project_id = params["id"].to_i
+  @projects = Project.find(project_id)
+
+  @volunteers = Volunteer.all
+  erb(:projects)
+end
+
+get('/projects/:id/update') do
+  # @id = params[:id].to_i
+  project_id = params["id"].to_i
+  @projects = Project.find(params.fetch("id").to_i)
+  new_title = params["new_title"]
+  @projects.update({:title => new_title})
   @volunteers = Volunteer.all
   erb(:projects)
 end
